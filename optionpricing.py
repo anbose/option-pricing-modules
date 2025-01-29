3df1cef46a04231a6bfb422b7cd6290ec493cc6d
Aritra Bose <43315603+anbose@users.noreply.github.com>
2025-01-29 23:42:26 +0100

    plot with sliders completed
    slider to change variables along with a reset button

--- optionpricing.py
+++ optionpricing.py
@@ -2,6 +2,7 @@ import numpy as np
 from scipy import special
 import matplotlib.pyplot as plt
 import copy
+from matplotlib.widgets import Button, Slider
 #plt.rcParams['text.usetex'] = True
 #plt.rcParams['font.family'] = 'serif'
 #plt.rcParams['font.serif'] = ['Computer Modern']
@@ -117,23 +118,58 @@ class Option:
             param_name = args[param_id]
             param_data = args[param_id+1]
             secondary_params[param_name] = param_data
+
+        param_setter = {
+            'underlying_price': lambda option,val : setattr(option, 'S', val),
+            'time': lambda option,val : setattr(option, 't', val),
+            'strike_price': lambda option,val : setattr(option, 'K', val),
+            'maturity_time': lambda option,val : setattr(option, 'T', val),
+            'volatility': lambda option,val : setattr(option, 'sigma', val),
+            'interest_rate': lambda option,val : setattr(option, 'r', val)
+        }
         
         fig,ax = plt.subplots(figsize=(10,6))
         plt.subplots_adjust(left=0.25,bottom=0.25)
         price_data = self.calculate_option_prices_for_parameters(primary_parameter,primary_data)
-        line, = ax.plot(primary_data,price_data)
-        ax.set_xlabel(' '.join(primary_parameter.split('_'),fontsize=16)
+        line, = ax.plot(primary_data,price_data,'o',markersize=3)
+        ax.set_xlabel(' '.join(primary_parameter.split('_')),fontsize=16)
         ax.set_ylabel('Option Price',fontsize=16)
         ax.grid(True)
 
-        silder_params = list(secondary_params.keys())
+        slider_params = list(secondary_params.keys())
+        slider_dict = {}
         for slider_id in range(len(secondary_params)):
-            silder_ax = fig.add_axes([0.25,0.1-i*0.05,0.65,0.03])      # [left, bottom, width, height]
-            slider_label = ' '.join(silder_params[i].split('_'))
-            slider_range = secondary_params[slider_params[i]]
-            slider_data = Slider(ax=slider_ax,label=slider_label,valmin=slider_range.min(),valmax=slider_range.max(),valinit=slider_range[0])
+            slider_ax = fig.add_axes([0.25,0.12-slider_id*0.05,0.65,0.03])      # [left, bottom, width, height]
+            slider_label = ' '.join(slider_params[slider_id].split('_'))
+            slider_range = secondary_params[slider_params[slider_id]]
+            slider_dict[slider_params[slider_id]] = Slider(ax=slider_ax,
+                                                            label=slider_label,valmin=slider_range.min(),
+                                                                valmax=slider_range.max(),valinit=slider_range[0])
+        current_params = {}
+        def update(val):
+            option_copy = copy.deepcopy(self)
+            for params in slider_dict:
+                param_setter[params](option_copy,slider_dict[params].val)
 
-        
+            prices = option_copy.calculate_option_prices_for_parameters(primary_parameter,primary_data)
+
+            line.set_ydata(prices)
+            ax.relim()
+            ax.autoscale_view()
+            fig.canvas.draw_idle()
+
+        for params in slider_dict:
+            slider_dict[params].on_changed(update)
+
+        reset_ax = fig.add_axes([0.1,0.25,0.05,0.03])
+        reset_button = Button(reset_ax,'Reset',color='0.85',hovercolor='0.95',useblit=True)
+
+        def reset(event):
+            for params in slider_dict:
+                slider_dict[params].reset()
+        reset_button.on_clicked(reset)
+
+        plt.show()
         
 
     def __repr__(self) -> str:
